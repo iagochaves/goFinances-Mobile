974958e674140bf6a57e831b09f27ec5db2741a7
iagochaves <iagovieirachaves@gmail.com>
2021-12-09 21:31:12 -0300

    Disabled Login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,7 @@ export function Routes() {
   console.log('user??', !!user);
   return (
     <NavigationContainer>
-      {user.id ? <AppRoutes /> : <AuthRoutes />}
+      {!user.id ? <AppRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
